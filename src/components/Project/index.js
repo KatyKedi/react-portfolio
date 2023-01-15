82be2ed45f6c6f9898ee4b4e1768709299c5bc17
KatyKedi <katykat5bros@gmail.com>
2023-01-14 20:16:09 -0800

    projects cards: added title, description, repo and deployment buttons

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Card, Button } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 import image1 from '../../assets/code-quiz.png';
 import image2 from '../../assets/workday-scheduler.png';
 import image3 from '../../assets/note-taker.png';
@@ -11,36 +11,42 @@ const projects = [
     {
       title: 'Code Quiz',
       img: image1,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://katykedi.github.io/code-quiz/',
       repo: 'https://github.com/KatyKedi/code-quiz'
     },
     {
       title: 'Workday Scheduler',
       img: image2,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://katykedi.github.io/workday-scheduler/',
       repo: 'https://github.com/KatyKedi/workday-scheduler'
     },
     {
       title: 'Note Taker',
       img: image3,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://note-taker-katykedi.herokuapp.com/',
       repo: 'https://github.com/KatyKedi/note-taker'
     },
     {
       title: 'Budget Tracker',
       img: image4,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://budget-tracker-katykedi.herokuapp.com/',
       repo: 'https://github.com/KatyKedi/budget-tracker'
     },
     {
       title: 'Apex Losers',
       img: image5,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://github.com/KatyKedi/apex-losers',
       repo: 'https://github.com/KatyKedi/apex-losers'
     },
     {
       title: 'TFT Compare',
       img: image6,
+      description: 'An app that lets you take a quiz to test your coding knowledge.',
       link: 'https://tft-compare.herokuapp.com/',
       repo: 'https://github.com/Atweaver7/tft-compare'
     }
@@ -48,7 +54,8 @@ const projects = [
 
 function Project() {
     return (
-      <>
+<Container>
+  <Row>
         {projects.map((project, index) => (
           <Col lg={4} md={6} className='my-2' key={index}>
             <Card>
@@ -60,11 +67,17 @@ function Project() {
                 key={index}
               >
               </Card.Img>
-              <Button variant="primary" href={project.repo} target="_blank">{project.title} Repo</Button>
+              <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.description}</Card.Text>
+                <Button className='text-light mx-2' variant="primary" href={project.repo} target="_blank">Repo</Button>
+                <Button className='text-light' variant="primary" href={project.link} target="_blank">Deployment</Button>
+              </Card.Body>        
             </Card>
           </Col>
         ))}
-      </>
+        </Row>
+      </Container>
     )
 }
 
