40eeb4713f8da5d34b12353c44fcdbb816c39cd6
KatyKedi <katykat5bros@gmail.com>
2023-01-14 22:29:05 -0800

    scss somehow works again even though nothing is different

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Button } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <footer className='text-light'>
-            <h2>Professional Profiles</h2>
-            <ul>
-                <li>Github: <Button href="https://github.com/KatyKedi" target='_blank'>KatyKedi</Button></li>
-                <li>LinkedIn: <Button href="https://www.linkedin.com/in/katy-vincent-95a7351a7/" target="_blank">Katy Vincent</Button></li>
-            </ul>
-        </footer>
+        <Container>
+            <Row>
+                <Col><a href='https://github.com/KatyKedi' target='_blank'><FaGithub className='text-primary'/></a></Col>
+                <Col><a href='https://www.linkedin.com/in/katy-vincent-95a7351a7/' target='_blank'><FaLinkedin className='text-primary'/></a></Col>
+            </Row>
+        </Container>
     );
 };
 
