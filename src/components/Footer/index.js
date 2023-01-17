6af87c685feeccc3d359bc01c9bcf2ef9fdd7328
KatyKedi <katykat5bros@gmail.com>
2023-01-16 16:28:05 -0800

    about section incorporated accordion and updated text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
 
 function Footer() {
     return (
         <Container>
-            <Row>
-                <Col><a href='https://github.com/KatyKedi' target='_blank'><FaGithub className='text-primary'/></a></Col>
-                <Col><a href='https://www.linkedin.com/in/katy-vincent-95a7351a7/' target='_blank'><FaLinkedin className='text-primary'/></a></Col>
+            <Row className='justify-content-center'>
+                <Col><a href='https://github.com/KatyKedi' target='_blank'><FaGithub className='h-75 w-75 text-primary'/></a></Col>
+                <Col><a href='https://www.linkedin.com/in/katy-vincent-95a7351a7/' target='_blank'><FaLinkedin className='h-75 w-75 text-primary'/></a></Col>
+                <Col><a href='mailto:katykat5bros@gmail.com' target='_blank'><HiOutlineMail className='h-75 w-75 text-primary'/></a></Col>
             </Row>
         </Container>
     );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,62 +8,62 @@ import image5 from '../../assets/apex-losers.png';
 import image6 from '../../assets/tft-compare.png';
 
 const projects = [
-    {
-      title: 'Code Quiz',
-      img: image1,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://katykedi.github.io/code-quiz/',
-      repo: 'https://github.com/KatyKedi/code-quiz'
-    },
-    {
-      title: 'Workday Scheduler',
-      img: image2,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://katykedi.github.io/workday-scheduler/',
-      repo: 'https://github.com/KatyKedi/workday-scheduler'
-    },
-    {
-      title: 'Note Taker',
-      img: image3,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://note-taker-katykedi.herokuapp.com/',
-      repo: 'https://github.com/KatyKedi/note-taker'
-    },
-    {
-      title: 'Budget Tracker',
-      img: image4,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://budget-tracker-katykedi.herokuapp.com/',
-      repo: 'https://github.com/KatyKedi/budget-tracker'
-    },
-    {
-      title: 'Apex Losers',
-      img: image5,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://github.com/KatyKedi/apex-losers',
-      repo: 'https://github.com/KatyKedi/apex-losers'
-    },
-    {
-      title: 'TFT Compare',
-      img: image6,
-      description: 'An app that lets you take a quiz to test your coding knowledge.',
-      link: 'https://tft-compare.herokuapp.com/',
-      repo: 'https://github.com/Atweaver7/tft-compare'
-    }
+  {
+    title: 'Code Quiz',
+    img: image1,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://katykedi.github.io/code-quiz/',
+    repo: 'https://github.com/KatyKedi/code-quiz'
+  },
+  {
+    title: 'Workday Scheduler',
+    img: image2,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://katykedi.github.io/workday-scheduler/',
+    repo: 'https://github.com/KatyKedi/workday-scheduler'
+  },
+  {
+    title: 'Note Taker',
+    img: image3,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://note-taker-katykedi.herokuapp.com/',
+    repo: 'https://github.com/KatyKedi/note-taker'
+  },
+  {
+    title: 'Budget Tracker',
+    img: image4,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://budget-tracker-katykedi.herokuapp.com/',
+    repo: 'https://github.com/KatyKedi/budget-tracker'
+  },
+  {
+    title: 'Apex Losers',
+    img: image5,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://github.com/KatyKedi/apex-losers',
+    repo: 'https://github.com/KatyKedi/apex-losers'
+  },
+  {
+    title: 'TFT Compare',
+    img: image6,
+    description: 'An app that lets you take a quiz to test your coding knowledge.',
+    link: 'https://tft-compare.herokuapp.com/',
+    repo: 'https://github.com/Atweaver7/tft-compare'
+  }
 ];
 
 function Project() {
-    return (
-<Container>
-  <Row>
+  return (
+    <Container>
+      <Row>
         {projects.map((project, index) => (
           <Col lg={4} md={6} className='my-2' key={index}>
             <Card>
-              <Card.Img 
-                variant="top" 
+              <Card.Img
+                variant="top"
                 src={project.img}
                 alt={project.title + ' image'}
-                onClick={() =>  window.open(project.link)}
+                onClick={() => window.open(project.link)}
                 key={index}
               >
               </Card.Img>
@@ -72,13 +72,13 @@ function Project() {
                 <Card.Text>{project.description}</Card.Text>
                 <Button className='text-light mx-2' variant="primary" href={project.repo} target="_blank">Repo</Button>
                 <Button className='text-light' variant="primary" href={project.link} target="_blank">Deployment</Button>
-              </Card.Body>        
+              </Card.Body>
             </Card>
           </Col>
         ))}
-        </Row>
-      </Container>
-    )
+      </Row>
+    </Container>
+  )
 }
 
 export default Project;
